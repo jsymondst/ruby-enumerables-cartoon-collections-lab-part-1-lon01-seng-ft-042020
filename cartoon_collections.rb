7df2e6a91e2505f3c9f090d6ecd48bfcde9e7fc4
Jamie Symonds-Tayler <jamie.s-t@outlook.com>
2020-04-14 15:29:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each do |name| 
-    puts name
+  array.each do |name|
+    puts "Hello #{name}"
   end
 end
 
@@ -11,4 +11,6 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do |item|
+
 end
